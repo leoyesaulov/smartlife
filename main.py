bebe17866db46b4be072c918ab57a71577b1ee57
Leo Yesaulov <52819769+leoyesaulov@users.noreply.github.com>
2025-02-25 21:33:17 +0100

    first small step towards victory

---
+++ main.py
@@ -0,0 +1,9 @@
+from pycololight import PyCololight
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+strip = PyCololight(device="strip", host=os.getenv("STRIP_IP"), dynamic_effects=True)
+
+strip.on = 0
