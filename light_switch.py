41a76b72219ec1424907cd181266e0045b8b8797
Leo Yesaulov <52819769+leoyesaulov@users.noreply.github.com>
2025-03-05 18:10:40 +0100

    fix timezones (why <= tho???)

--- light_switch.py
+++ light_switch.py
@@ -9,14 +9,18 @@ from pycololight import PyCololight
 load_dotenv()
 strip = PyCololight(device="strip", host=os.getenv("STRIP_IP"), dynamic_effects=True)
 city = LocationInfo("Munich", "Germany", "Europe/Berlin", 48.13743, 11.57549)
+strip.on = 0
+utc = datetime.timezone(datetime.timedelta(hours=0))
+munich = datetime.timezone(datetime.timedelta(hours=+1))
 
-sunset = sun(city.observer)["sunset"]
-sunrise = sun(city.observer)["sunrise"]
-now = datetime.datetime.now().astimezone()
 
 def check():
-    if now >= sunset - datetime.timedelta(minutes=30) and strip.brightness == 0:
-        print("Turning lights on at: " + now.strftime("%d.%b.%Y %H:%M:%S"))
+    sunset = sun(city.observer)["sunset"].astimezone(munich)
+    sunrise = sun(city.observer)["sunrise"].astimezone(munich)
+    now = datetime.datetime.now().astimezone(munich)
+
+    if now >= sunset - datetime.timedelta(minutes=30) and (strip.brightness == 0 or strip.brightness is None):
+        print("Turning lights on with previous brightness: ", strip.brightness, " at: " + now.astimezone().strftime("%d.%b.%Y %H:%M:%S"))
         strip.on = 25
     if now <= sunrise + datetime.timedelta(minutes=30) and strip.brightness > 0:
         print("Turning lights off at: " + now.strftime("%d.%b.%Y %H:%M:%S"))
