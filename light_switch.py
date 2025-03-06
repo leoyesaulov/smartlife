d84541c06ce4705ab20a1196c4e0579a148dac6a
Leo Yesaulov <52819769+leoyesaulov@users.noreply.github.com>
2025-03-06 15:19:07 +0100

    fix stupidity (why <= tho???)

--- light_switch.py
+++ light_switch.py
@@ -9,7 +9,6 @@ from pycololight import PyCololight
 load_dotenv()
 strip = PyCololight(device="strip", host=os.getenv("STRIP_IP"), dynamic_effects=True)
 city = LocationInfo("Munich", "Germany", "Europe/Berlin", 48.13743, 11.57549)
-strip.on = 0
 utc = datetime.timezone(datetime.timedelta(hours=0))
 munich = datetime.timezone(datetime.timedelta(hours=+1))
 
@@ -18,10 +17,11 @@ def check():
     sunset = sun(city.observer)["sunset"].astimezone(munich)
     sunrise = sun(city.observer)["sunrise"].astimezone(munich)
     now = datetime.datetime.now().astimezone(munich)
+    strip.state()
 
-    if now >= sunset - datetime.timedelta(minutes=30) and (strip.brightness == 0 or strip.brightness is None):
+    if now >= sunset - datetime.timedelta(minutes=30) and not strip.on:
         print("Turning lights on with previous brightness: ", strip.brightness, " at: " + now.astimezone().strftime("%d.%b.%Y %H:%M:%S"))
         strip.on = 25
-    if now <= sunrise + datetime.timedelta(minutes=30) and strip.brightness > 0:
+    if now <= sunrise + datetime.timedelta(minutes=30) and strip.on:
         print("Turning lights off at: " + now.strftime("%d.%b.%Y %H:%M:%S"))
         strip.on = 0
