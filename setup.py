07c87ee911e6588ffae6ae07a4f8d9bbbb5fef46
Leo Yesaulov <52819769+leoyesaulov@users.noreply.github.com>
2025-04-02 16:50:30 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from data_handler import DataHandler
 __data_handler = DataHandler()
 __strip_ip = __data_handler.get('strip_ip')
 
-while __strip_ip == "":
+while not __strip_ip:
     __strip_ip = input("Please enter ip of your cololight strip: ")
 
 __data_handler.write("strip_ip", __strip_ip)
